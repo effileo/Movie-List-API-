22f5c8b12867b10e8fcb0856fdadded373fc36db
effileo <fitaalemayehu12@gmail.com>
2026-01-22 21:32:10 +0300

    Adding data to database

--- src/controllers/authcontroller.js
+++ src/controllers/authcontroller.js
@@ -1,6 +1,36 @@
+import prisma from "../config/db.js"
+import bcrypt from "bcryptjs"
 const register = async (req, res) => {
-    const body = req.body;
-    res.json(body);
-}
+    const { name, email, password } = req.body;
+    const userExist = await prisma.user.findUnique({
+        where: {
+            email: email,
+        }
+    });
+    if (userExist) {
+        return res.status(400).json({
+            message: "user already exists"
+        });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt)
+    const user = await prisma.user.create(
+        {
+            data: {
+                name,
+                email,
+                password: hashedPassword
+            },
+        }
+    );
+    res.status(201).json({
+        status: "success",
+        data: {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+        }
+    });
+};
 
 export { register };
