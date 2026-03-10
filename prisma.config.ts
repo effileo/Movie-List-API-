c43674c6b266e215810a98359a8a300ff71418f0
effileo <fitaalemayehu12@gmail.com>
2026-03-10 11:01:35 +0300

    update'o

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from '@prisma/config';
-import dotenv from 'dotenv';
+import { defineConfig } from "@prisma/config";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 export default defineConfig({
-  schema: 'prisma/schema.prisma',
+  engine: "classic",
+  schema: "prisma/schema.prisma",
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
   },
 });
