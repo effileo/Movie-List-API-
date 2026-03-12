176e9a3741d852b492806a1fa2e01040602330af
effileo <fitaalemayehu12@gmail.com>
2026-03-12 17:37:02 +0300

    some updates on the logout still

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { register } from '../controllers/authcontroller.js';
 import {login} from '../controllers/authcontroller.js'; 
+import {logout} from '../controllers/authcontroller.js';
 const router = express.Router();
 
 router.post('/register' , register);
 router.post('/login' , login);
+router.post('/logout' , logout);
     export default router;     
     
