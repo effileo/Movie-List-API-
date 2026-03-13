261c72930823dc4f6e1a7d1764098d5f5b8aab69
effileo <fitaalemayehu12@gmail.com>
2026-03-13 13:01:56 +0300

    this is adding movie to the watchlist

--- src/controllers/watchlistcontroller.js
+++ src/controllers/watchlistcontroller.js
@@ -1,5 +1,10 @@
-
+import {prisma} from "../config/db"
 const addToWatchList= async(req, res) =>{
     const {movieID, status, rating, notes}= req.body
-
+// verify movie exist in the movie table
+const movieExist = await prisma.movie.findUnique({
+    where: {
+        id: movieID,
+    }
+})
 }
