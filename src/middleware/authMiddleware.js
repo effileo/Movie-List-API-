e522313eb5ca7d34916eb901c2432abf6c1eaba0
effileo <fitaalemayehu12@gmail.com>
2026-03-16 10:24:48 +0300

    carrying tokens from headers or cookies

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -3,5 +3,13 @@ import prisma from "../config/db.js";
 
 const authMiddleware = async ()=>{
  console.log("authMiddleware called");
+ let token;
+ if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")
+){
+    token = req.headers.authorization.split("")[1];
+}
+else if (req.cookies.token){
+    token = req.cookies.token;
+}
 };
 export default authMiddleware;
